542094598c06d17b21ba7db519e2a918c3922f4f
Carson Sorenson <carsonwsorenson@gmail.com>
2019-11-18 22:09:01 -0700

    added most of the training logic

--- load_images.py
+++ load_images.py
@@ -27,15 +27,18 @@ def build_image_matrix(images, grid_size, output_file):
     cv2.imwrite(output_file, image_matrix)
 
 
-def load_real_images(data_dir, image_size, attributes=None):
+def load_real_images(data_dir, image_size, dataset_size, attributes=None):
     files = load_attributes_wrapper(data_dir, attributes)
     images = []
-    for file in files:
-        img = cv2.imread(file)
-        cropped_img = img[55:55+128, 30:30+128]
-        resized_im = cv2.resize(cropped_img, (image_size, image_size))
-        normalized_img = (resized_im / 127.5) - 1.0
-        images.append(normalized_img)
+    for i, file in enumerate(files):
+        if i == dataset_size:
+            break
+        else:
+            img = cv2.imread(file)
+            cropped_img = img[55:55+128, 30:30+128]
+            resized_im = cv2.resize(cropped_img, (image_size, image_size))
+            normalized_img = (resized_im / 127.5) - 1.0
+            images.append(normalized_img)
     return images
 
 
